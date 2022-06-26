2cdd047d85ab171a925fcca071a598f7de255573
Tejas <tthakare73@gmail.com>
2022-06-26 10:51:38 +0530

    New Changes `26-06-2022_10:51:38.52`

--- src/utils/const.js
+++ src/utils/const.js
@@ -9,6 +9,6 @@ export const CLUSTER ="devnet";
 export const SOLANA_HOST = clusterApiUrl(NETWORK_MAIN);
 // export const SOLANA_HOST = clusterApiUrl("devnet")
 
-export const TIKTOK_PROGRAM_ID = new PublicKey('Az4edEtU6JtghfueC4hS7Fo5fG3evPY5VUt6YbNHmhaN');
+export const TIKTOK_PROGRAM_ID = new PublicKey('8zU1ZS542XJEf3D2FJwRkiC2zxJQcAuCPs3RmJf2Lu6K');
 
 export const TIKTOK_IDL = tiktok;
