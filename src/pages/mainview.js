1bb2bd89fc74d59b1dbc54a32619f673e5541ddd
Tejas <tthakare73@gmail.com>
2022-06-28 10:22:34 +0530

    New Changes `28-06-2022_10:22:34.17`

--- src/pages/mainview.js
+++ src/pages/mainview.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './style.css'
-// import home from '../assets/home.svg'
+import baseicon1 from '../assets/baseIcon(1).png'
+import baseicon2 from '../assets/baseIcon(2).png'
+import baseicon3 from '../assets/baseIcon(3).png'
+import baseicon4 from '../assets/baseIcon(4).png'
+
 
 export default function Mainview() {
   
@@ -14,10 +18,12 @@ export default function Mainview() {
         <div className='w-[100%] h-[100%] snap-center bg-purple-500 flex justify-center items-center'>TikTok</div>
         
       </div>
-      <div className='absolute bottom-0 md:bottom-[5%] w-[100%] md:w-[40%] lg:w-[35%] xl:w-[25%] md:rounded-b-xl text-white w-full bg-red-100 flex justify-center'>
-        <div onClick={()=>{}}>
-          <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"/></svg>
-        </div>
+      <div className='absolute bottom-0 md:bottom-[5%] w-[100%] md:w-[40%] lg:w-[35%] xl:w-[25%] md:rounded-b-xl text-white w-full bg-black flex justify-between py-2 px-7 items-center'>
+        <img className="w-[9%]" src={baseicon4} />
+        <img className="w-[9%]" src={baseicon1} />
+        <div className="bg-white w-[11%] h-6 rounded-md text-black flex justify-center items-center text-2xl pb-1">+</div>
+        <img className="w-[9%]" src={baseicon2} />
+        <img className="w-[9%]" src={baseicon3} />
       </div>
     </div>
   )
