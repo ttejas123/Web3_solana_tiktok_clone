99ff4493a7c2cff0b71f6a45a6db1ad0fa22f37c
Tejas <tthakare73@gmail.com>
2022-06-28 11:30:08 +0530

    New Changes `28-06-2022_11:30:08.80`

--- src/pages/createVideo.js
+++ src/pages/createVideo.js
@@ -4,23 +4,27 @@ function Alloutes(props) {
   const [description, setDescription] = useState("");
   const [url, setUrl] = useState("");
   return (
-    <div className="bg-red-900 h-[100vh] flex justify-center items-center w-[100%]">
-        <div className="bg-white w-[90%] md:w-[45%] lg:w-[40%] xl:w-[30%] rounded-lg flex flex-col justify-center items-center py-5">
-        
-        <div className="font-bold text-xl">Upload Tiktok</div>
-        
-        <div className="my-5 flex flex-col justify-center items-center w-[100%]">
-            <div className="text-md text-left w-[90%]">Description:</div>
-            <input className="bg-gray-200 rounded-md py-[8px] mb-1 w-[90%]" value={description} onChange={(e)=> setDescription(e.target.value)} />
-            <div className="text-md text-left w-[90%]">Video Url:</div>
-            <input className="bg-gray-200 rounded-md py-[8px] px-[3px] w-[90%]" value={url} onChange={(e)=> setUrl(e.target.value)} />
+    <>
+        <div className='h-[100vh] flex justify-center items-center w-[100%] bg-transparent' onClick={() => props.setVisible(!props.visible)}></div>
+        <div className="flex justify-center items-center w-[100%] absolute top-[30%]">
+            <div className="bg-white w-[90%] rounded-lg flex flex-col justify-center items-center py-5">
+            
+            <div className="font-bold text-xl">Upload Tiktok</div>
+            
+            <div className="my-5 flex flex-col justify-center items-center w-[100%]">
+                <div className="text-md text-left w-[90%]">Description:</div>
+                <input className="bg-gray-200 rounded-md py-[8px] mb-1 w-[90%]" value={description} onChange={(e)=> setDescription(e.target.value)} />
+                <div className="text-md text-left w-[90%]">Video Url:</div>
+                <input className="bg-gray-200 rounded-md py-[8px] px-[3px] w-[90%]" value={url} onChange={(e)=> setUrl(e.target.value)} />
+            </div>
+            
+            <div className="bg-purple-700 rounded-lg w-[80%] text-center text-white py-[9px] font-bold cursor-pointer " onClick={() => {
+                // props.signup(description, url)
+                props.setVisible(!props.visible)
+            }}>Upload</div>
+            </div>
         </div>
-        
-        <div className="bg-purple-700 rounded-lg w-[80%] text-center text-white py-[9px] font-bold cursor-pointer " onClick={() => {
-            props.signup(description, url)
-          }}>Upload</div>
-        </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/mainview.js
+++ src/pages/mainview.js
@@ -11,7 +11,7 @@ export default function Mainview() {
   const[visible, setVisible] = useState(false)
   return (
     <div className="flex justify-center items-center h-[100vh] bg-stone-900 relative">
-      {visible && (<div className="absolute bg-red-100 w-[100%] md:w-[40%] lg:w-[35%] xl:w-[25%]" onClick={() => setVisible(!visible)}><CreateVideo /></div>)}
+      {visible && (<div className="absolute  bg-transparent h-[100%] w-[100%] md:w-[40%] lg:w-[35%] xl:w-[25%]"><CreateVideo setVisible={setVisible} visible={visible} /></div>)}
       <div className="h-[100%] md:h-[90%] bg-white w-[100%] md:w-[40%] lg:w-[35%] xl:w-[25%] md:rounded-xl overflow-auto snap-mandatory snap-y scroll-mandatory-here">
         <div className='w-[100%] h-[100%] snap-center bg-red-500 flex justify-center items-center'>TikTok</div>
         <div className='w-[100%] h-[100%] snap-center bg-green-500 flex justify-center items-center'>TikTok</div>
@@ -21,11 +21,11 @@ export default function Mainview() {
         
       </div>
       <div className='absolute bottom-0 md:bottom-[5%] w-[100%] h-[8%] md:w-[40%] lg:w-[35%] xl:w-[25%] md:rounded-b-xl text-white w-full bg-black flex justify-between py-2 px-7 items-center'>
-        <img className="w-[9%]" src={baseicon4} />
-        <img className="w-[9%]" src={baseicon1} />
-        <div onClick={() => setVisible(!visible)} className="bg-white w-[11%] h-6 rounded-md text-black flex justify-center items-center text-2xl">+</div>
-        <img className="w-[9%]" src={baseicon2} />
-        <img className="w-[9%]" src={baseicon3} />
+        <img className="w-[9%] cursor-pointer" src={baseicon4} />
+        <img className="w-[9%] cursor-pointer" src={baseicon1} />
+        <div onClick={() => setVisible(!visible)} className="bg-white cursor-pointer w-[11%] h-6 rounded-md text-black flex justify-center items-center text-2xl">+</div>
+        <img className="w-[9%] cursor-pointer" src={baseicon2} />
+        <img className="w-[9%] cursor-pointer" src={baseicon3} />
       </div>
     </div>
   )
